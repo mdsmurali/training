78c746f14aa35a8cf1ee31461db641d2d4de72fb
MuraliMDS <mdsmurali@outlook.com>
2018-05-17 23:48:29 +0530

    version2

--- myfile.java
+++ myfile.java
@@ -3,6 +3,7 @@ class myfile
 public static void main(String args[])
       {
       
-      System.out.println("Hello DevOps");
+        System.out.println("Hello DevOps");
+	System.out.println("Hello DevOps Ver2");
       }
 }
